c2beca09cdaec723f3562c6d3a3bd4462f755c41
Aurélien Clergeot <aureli1.clergeot@laposte.net>
2021-08-21 13:49:49 +0200

    Add some tests

--- app/src/androidTest/java/ch/epfl/reminday/ui/view/DatePickerViewInstrumentedTest.kt
+++ app/src/androidTest/java/ch/epfl/reminday/ui/view/DatePickerViewInstrumentedTest.kt
@@ -1,14 +1,17 @@
 package ch.epfl.reminday.ui.view
 
+import android.content.Context
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import ch.epfl.reminday.R
 import ch.epfl.reminday.SafeViewScenario
 import ch.epfl.reminday.testutils.NumberPickerTestUtils
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,9 +25,32 @@ class DatePickerViewInstrumentedTest {
     private val onMonth: ViewInteraction get() = onView(withId(R.id.month))
     private val onYear: ViewInteraction get() = onView(withId(R.id.year))
 
+
+    private fun factory(yearEnabled: Boolean) = { context: Context ->
+        DatePickerView(context).apply { isYearEnabled = yearEnabled }
+    }
+
+    @Test
+    fun yearIsInvisibleWhenDisabled() {
+        SafeViewScenario.launchInHiltContainer(factory(yearEnabled = false)) {
+            onDay.check(matches(isDisplayed()))
+            onMonth.check(matches(isDisplayed()))
+            onYear.check(matches(not(isDisplayed())))
+        }
+    }
+
+    @Test
+    fun yearIsVisibleWhenEnabled() {
+        SafeViewScenario.launchInHiltContainer(factory(yearEnabled = true)) {
+            onDay.check(matches(isDisplayed()))
+            onMonth.check(matches(isDisplayed()))
+            onYear.check(matches(isDisplayed()))
+        }
+    }
+
     @Test
     fun dayIsConstrainedDependingOnMonthAndYear() {
-        SafeViewScenario.launchInHiltContainer({ context -> DatePickerView(context) }) {
+        SafeViewScenario.launchInHiltContainer(factory(yearEnabled = true)) {
             onDay.perform(NumberPickerTestUtils.setValueByJumping(31))
             onMonth.perform(NumberPickerTestUtils.setValueByJumping(12))
             onYear.perform(NumberPickerTestUtils.setValueByJumping(1960))
@@ -34,6 +60,21 @@ class DatePickerViewInstrumentedTest {
 
             onYear.perform(NumberPickerTestUtils.setValueIncrementally(1961))
             onDay.check(matches(NumberPickerTestUtils.withValue(28)))
+        }
+    }
+
+    @Test
+    fun februaryHas29DaysWhenYearIsDisabled() {
+        SafeViewScenario.launchInHiltContainer(factory(yearEnabled = true)) { scenario ->
+            onDay.perform(NumberPickerTestUtils.setValueByJumping(29))
+            onMonth.perform(NumberPickerTestUtils.setValueByJumping(2))
+            onYear.perform(NumberPickerTestUtils.setValueByJumping(1961))
+
+            onDay.check(matches(NumberPickerTestUtils.withValue(28)))
+            scenario.onView { it.isYearEnabled = false }
+
+            onDay.perform(NumberPickerTestUtils.setValueIncrementally(29))
+            onDay.check(matches(NumberPickerTestUtils.withValue(29)))
         }
     }
 }
