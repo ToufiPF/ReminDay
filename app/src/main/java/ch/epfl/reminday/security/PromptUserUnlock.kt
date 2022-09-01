7e0cd5cf418bdfa3a940ed048be9fc4a6d6ee8a9
Aurélien Clergeot <aureli1.clergeot@laposte.net>
2022-09-01 15:27:03 +0200

    Missing doc & theme

--- app/src/main/java/ch/epfl/reminday/security/PromptUserUnlock.kt
+++ app/src/main/java/ch/epfl/reminday/security/PromptUserUnlock.kt
@@ -2,16 +2,21 @@ package ch.epfl.reminday.security
 
 import androidx.fragment.app.FragmentActivity
 
-
+/**
+ * Interface to handle user authentication (via biometrics/password).
+ */
 interface PromptUserUnlock {
 
     /**
-     * Returns whether the device supports authentication (it has a password set up)
+     * Returns whether the device supports authentication.
+     * @return true if authentication is supported (i.e.
      */
     fun canAuthenticate(): Boolean
 
     /**
-     * Prompts the user to authenticate. Returns true if successful.
+     * Prompts the user to authenticate.
+     * @param activity [FragmentActivity] the prompt should be drawn upon
+     * @return true if successful, false if user failed to authenticate/cancelled the prompt.
      */
     suspend fun authenticate(activity: FragmentActivity): Boolean
 }
