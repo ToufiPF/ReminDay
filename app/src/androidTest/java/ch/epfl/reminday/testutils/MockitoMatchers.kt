d0a623f44185a12e7e429e8e3fee57e394b52935
Aurélien Clergeot <aurelien.clergeot@elca.ch>
2025-08-29 18:30:22 +0200

    Fix tests (WiP)

--- app/src/androidTest/java/ch/epfl/reminday/testutils/MockitoMatchers.kt
+++ app/src/androidTest/java/ch/epfl/reminday/testutils/MockitoMatchers.kt
@@ -1,13 +1,8 @@
 package ch.epfl.reminday.testutils
 
-import org.hamcrest.BaseMatcher
-import org.hamcrest.Description
-import org.hamcrest.Matcher
+import ch.epfl.reminday.testutils.MockitoMatchers.any
 import org.mockito.ArgumentCaptor
-import org.mockito.ArgumentMatcher
-import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
-import org.mockito.internal.matchers.VarargMatcher
 
 /**
  * Android's Mockito matchers are made for java, causing several problems with matchers.
@@ -27,37 +22,6 @@ object MockitoMatchers {
      * For symmetry with [any], returns a Mockito matcher that supports nullable values.
      */
     inline fun <reified T> anyNullable(): T? = Mockito.nullable(T::class.java)
-
-    /**
-     * Matches any vararg of type T.
-     * Use with the spread operator <pre>'*'</pre>.
-     */
-    inline fun <reified T> anyVararg(): Array<out T> {
-        return VarArgMatcher.varArgThat(object : BaseMatcher<Array<out T>>() {
-            override fun describeTo(description: Description?) {
-                description?.appendText("matches anything")
-            }
-
-            override fun matches(item: Any?): Boolean = true
-        })
-    }
-
-    /**
-     * Matcher for varargs.
-     */
-    class VarArgMatcher<T> @PublishedApi internal constructor(
-        private val hamcrestMatcher: Matcher<Array<out T>>
-    ) : ArgumentMatcher<Array<out T>>, VarargMatcher {
-
-        companion object {
-            inline fun <reified T> varArgThat(hamcrestMatcher: Matcher<Array<out T>>): Array<out T> {
-                ArgumentMatchers.argThat(VarArgMatcher(hamcrestMatcher))
-                return Array(0) { null as T }
-            }
-        }
-
-        override fun matches(argument: Array<out T>): Boolean = hamcrestMatcher.matches(argument)
-    }
 
     /**
      * Android's Mockito argument captors are made for Java,
